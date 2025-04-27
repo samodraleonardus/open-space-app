41917d045ecf3aa16f90228103634a67a19d15b9
Samodra <samodraleonardus@gmail.com>
2025-04-27 21:32:09 +0700

    make test pass two

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -26,7 +26,7 @@ function LoginPage() {
           ,
           {' '}
           <br />
-          Through Open Space App.
+          Through Open Space App App.
         </h2>
 
         <LoginInput login={onLogin} />
